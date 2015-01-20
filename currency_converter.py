c51e5fcc0df41083a9c6593bf4f6f27510e1d65b
Dean Fitzgerald <classicmurph@gmail.com>
2015-01-20 16:55:37 -0500

    Test for conversion.

--- currency_converter.py
+++ currency_converter.py
@@ -3,3 +3,11 @@ def convert(rates, value, from_code, to_code):
         currency"""
     if from_code == to_code:
         return str(value) + " " + to_code
+    else:
+        returned_list = [(origin, destination, rate) for (origin, destination, rate)
+                         in rates if from_code == origin]
+        returned_tuple = returned_list[0]
+        returned_rate = returned_tuple[2]
+        new_value = returned_rate * value
+        return str(new_value) + " " + to_code
+        
--- test_currency_converter.py
+++ test_currency_converter.py
@@ -4,3 +4,6 @@ rates = [("USD", "EUR", 0.74), ("EUR", "JPY", 145.949)]
 
 def test_one_to_one():
     assert cc.convert(rates, 1, "USD", "USD") == "1 USD"
+
+def test_conversion_simple():
+    assert cc.convert(rates, 1, "USD", "EUR") == "0.74 EUR"
