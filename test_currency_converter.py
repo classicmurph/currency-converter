52e510afc2708704c3953fed9d0b17e5c5d6ecb0
Dean Fitzgerald <classicmurph@gmail.com>
2015-01-20 17:02:38 -0500

    test 2 dollars to EUR

--- test_currency_converter.py
+++ test_currency_converter.py
@@ -5,8 +5,14 @@ rates = [("USD", "EUR", 0.74), ("EUR", "JPY", 145.949)]
 def test_one_to_one():
     assert cc.convert(rates, 1, "USD", "USD") == "1 USD"
 
+
 def test_conversion_simple():
     assert cc.convert(rates, 1, "USD", "EUR") == "0.74 EUR"
 
+
 def test_conversion():
     assert cc.convert(rates, 0.5, "USD", "EUR") == "0.37 EUR"
+
+
+def test_conversion_2_dollars():
+    assert cc.convert(rates, 2, "USD", "EUR") == "1.48 EUR"
