cb03fc554e50336e6a70c732a4292c16d3e038f2
Dean Fitzgerald <classicmurph@gmail.com>
2015-04-07 11:33:09 -0400

    restructure

---
+++ more_tests.py
@@ -0,0 +1,10 @@
+
+
+
+    # returned_list = [(origin, destination, rate) for (origin, destination, rate)
+    #                  in rates if from_code == origin]
+    # returned_tuple = returned_list[0]
+    # returned_rate = returned_tuple[2]
+    # new_value = returned_rate * value
+    # return new_value
+    # #return ("{} {}".format(return_value, to_code))
